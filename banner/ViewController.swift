2ad46af6886c625e47cf60e8dcaf6a11d54925c5
uuwani <paulchoi9502@gmail.com>
2020-01-19 16:04:30 +0900

    Initial Commit

---
+++ banner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  banner
+//
+//  Created by Wanni on 19/01/2020.
+//  Copyright © 2020 Wanni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
